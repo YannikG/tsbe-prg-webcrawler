f2bb9d32070fbf1ba0ca21ed54e73694efa819fd
Yannik Gartmann <gartmann.yannik@gmail.com>
2022-12-23 15:29:29 +0100

    remove not needed delegates (happy!)

--- YannikG.TSBE.Webcrawler.Core/Processors/ProcessorDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using YannikG.TSBE.Webcrawler.Core.Pipelines.Configs;
-
-namespace YannikG.TSBE.Webcrawler.Core.Processors
-{
-    public delegate Task<ProcessorResult> ProcessorCallback<TInput, TPipelineSettings>(TInput? input, TPipelineSettings pipelineSettings) where TInput : class where TPipelineSettings : IPipelineSettings;
-
-    public delegate Task<ProcessorResult> ProcessorNextCallback<TInput>(TInput? input) where TInput : class;
-}
