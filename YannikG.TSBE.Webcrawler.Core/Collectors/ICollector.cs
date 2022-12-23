01b1ef279ff64c602023c3aa84ae8cd131d591bf
Yannik Gartmann <gartmann.yannik@gmail.com>
2022-12-23 16:03:44 +0100

    adding more comments to describe the pipeline construct better.

--- YannikG.TSBE.Webcrawler.Core/Collectors/ICollector.cs
+++ YannikG.TSBE.Webcrawler.Core/Collectors/ICollector.cs
@@ -2,8 +2,18 @@
 
 namespace YannikG.TSBE.Webcrawler.Core.Collectors
 {
+    /// <summary>
+    /// generic interface for a new collector that should be used in a <see cref="Pipelines.Pipeline{TInput, TPipelineSettings}"/>
+    /// </summary>
+    /// <typeparam name="TInput"></typeparam>
+    /// <typeparam name="TPipelineSettings"></typeparam>
     public interface ICollector<TInput, TPipelineSettings> where TInput : class where TPipelineSettings : IPipelineSettings
     {
+        /// <summary>
+        /// generic method that returns a <see cref="ICollection{TInput}"/> and takes <paramref name="pipelineSettings"/> with settings.
+        /// </summary>
+        /// <param name="pipelineSettings"></param>
+        /// <returns></returns>
         public Task<ICollection<TInput>> CollectAsync(TPipelineSettings pipelineSettings);
     }
 }
--- YannikG.TSBE.Webcrawler.Core/Processors/IProcessor.cs
+++ YannikG.TSBE.Webcrawler.Core/Processors/IProcessor.cs
@@ -2,14 +2,19 @@
 
 namespace YannikG.TSBE.Webcrawler.Core.Processors
 {
+    /// <summary>
+    /// generic interface for a new processor that should be used in a <see cref="Pipelines.Pipeline{TInput, TPipelineSettings}"/>
+    /// </summary>
+    /// <typeparam name="TInput"></typeparam>
+    /// <typeparam name="TPipelineSettings"></typeparam>
     public interface IProcessor<TInput, TPipelineSettings> where TInput : class where TPipelineSettings : IPipelineSettings
     {
         /// <summary>
-        /// Generic method for to process an object.
+        /// method that processes an object of the type <typeparamref name="TInput"/> with settings provided trough <paramref name="pipelineSettings"/> and returns a valid <see cref="ProcessorResult"/>
         /// </summary>
         /// <param name="input"></param>
         /// <param name="pipelineSettings"></param>
-        /// <param name="next"></param>
+        /// <returns></returns>
         public Task<ProcessorResult> ProcessAsync(TInput? input, TPipelineSettings pipelineSettings);
     }
 }
